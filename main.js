c56c6485bb1a1c5fc01419ae797105817c2c16a3
Sam Mudick <smudick@gmail.com>
2020-07-23 18:43:41 -0500

    created array of objects for each pet

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log("loggin it!")
+const pets = [
+    {
+        image: "https://s3.amazonaws.com/petcentral.com/wp-content/uploads/2016/09/01160419/black-cat-1-940x503.jpg",
+        name: "Mocha",
+        color: "black",
+        specialSkill: "fetch",
+        typeOfPet: "cat"
+      },
+      {
+        image: "https://pixy.org/download/4663301/",
+        name: "Kitters",
+        color: "Spotted",
+        specialSkill: "Sleeping",
+        typeOfPet: "cat"
+      },
+      {
+        image: "https://i.pinimg.com/originals/1d/26/14/1d26144b04873f9a13ee05c0ae8d583f.jpg",
+        name: "Chomper",
+        color: "Green",
+        specialSkill: "Chomping",
+        typeOfPet: "dino"
+      },
+      {
+        image: "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/s960x960/93366679_10221584510259882_1653151457899184128_o.jpg?_nc_cat=102&amp;_nc_sid=110474&amp;_nc_ohc=Uvlk5CYAhrwAX-3RMpY&amp;_nc_ht=scontent-atl3-1.xx&amp;_nc_tp=7&amp;oh=2a5eabade88f74514544ac279ca9f77c&amp;oe=5F3F984C",
+        name: "Souffle",
+        color: "Brown",
+        specialSkill: "Spin",
+        typeOfPet: "dog"
+      },
+      {
+        image: "https://images.creativemarket.com/0.1.0/ps/2438977/300/200/m2/fpc/wm0/kgdhbglux2txdxhfk4pgijvt454ybpmzocyw6evyd4zhw5ijjjpguadsz34cgjfh-.jpg?1490100724&s=6f95e764e2a1d7bb2e37b089edc594b8",
+        name: "BK",
+        color: "Orange",
+        specialSkill: "Boxing",
+        typeOfPet: "cat"
+      },
+      {
+        image: "https://vignette.wikia.nocookie.net/landbeforetime/images/0/0b/Littlefoot_IV.png/revision/latest?cb=20171227183122",
+        name: "Littlefoot",
+        color: "purple",
+        specialSkill: "javascript",
+        typeOfPet: "dino"
+      }
+]
